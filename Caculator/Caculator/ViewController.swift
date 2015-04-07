509d65ee83c7a5d7025a00b4537ea3a3f44fae85
senghuot <senghuotlim@gmail.com>
2015-04-06 17:10:56 -0700

    Adding a calculator ap

---
+++ Caculator/Caculator/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  Caculator
+//
+//  Created by Senghuot Lim on 4/4/15.
+//  Copyright (c) 2015 Home Brew. All rights reserve d.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    var usrIsInTheMiddleOfTypingANumber = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+
+        if usrIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            usrIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    
+    @IBAction func operate(sender: UIButton) {
+        var operation = sender.currentTitle!
+        switch operation {
+            case "×": performOperation {$0 * $1}
+            case "÷": performOperation {$1 / $0}
+            case "÷": performOperation {$0 + $1}
+            case "÷": performOperation {$1 - $0}
+            case "√": performOperation {sqrt($0)}
+            default: break
+        }
+    }
+    
+    
+    @IBAction func clear(sender: UIButton) {
+        operandStack.removeAll()
+        display.text = "0"
+    }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation:(Double) -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func multiply(op1: Double, op2: Double) -> Double {
+        return op1 * op2
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        usrIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            usrIsInTheMiddleOfTypingANumber = false
+        }
+    }
+}
+
+ 
