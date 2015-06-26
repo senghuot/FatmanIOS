87f46ff12a125b347559ece557add910084bf9c6
senghuot <senghuotlim@gmail.com>
2015-06-25 21:47:55 -0700

    Adding more stuff

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,4 +3,10 @@ import UIKit
 import Darwin
 
 var str = "Hello, playground"
-var str: String = "Explicit datatype declared, but it is not required."
+var str2: String = "Explicit datatype declared, but it is not required."
+
+var num = 92312321
+var num2: Int = 92312321
+
+println("Num++ = \(num++)")
+println("++Num = \(++num2)")
